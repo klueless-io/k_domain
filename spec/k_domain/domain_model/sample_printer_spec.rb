c45a752b815cc29d8c1092d7a80ac56dff1dce3a
David Cruwys <david@ideasmen.com.au>
2021-09-29 09:57:48 +1000

    add printers for domain_model models, erd_files and dictionary

---
+++ spec/k_domain/domain_model/sample_printer_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+RSpec.describe 'KDomain::DomainModelSchema::SamplePrinter' do
+  include KLog::Logging
+
+  include_examples :load_domain_model
+
+  # let(:load_db_schema_file) { 'spec/sample_output/raw_db_schema/schema.json' }
+
+  def show_length(array)
+    return '' if array.nil?
+
+    length = array.length
+    length.zero? ? '' : length
+  end
+
+  context 'print tables' do
+    let(:graph) do
+      {
+        models: {
+          title: 'Models',
+          take: :all,
+          columns: [
+            :name,
+            :name_plural,
+            :table_name,
+            :type,
+            { pk:               { display_method: ->(row) { row.pk.name                            } } },
+            { pk_type:          { display_method: ->(row) { row.pk.type                            } } },
+            { pk_exist:         { display_method: ->(row) { row.pk.exist                           } } },
+            { ruby_exist:       { display_method: ->(row) { row.erd_location.exist                 } } },
+            # TODO: :main_key,
+            # TODO: { traits:           { display_method: -> (row) { row.traits.join(',')                   } } },
+            { column_count:     { display_method: ->(row) { show_length(row.columns)               } } },
+            { for_data:         { display_method: ->(row) { show_length(row.columns_data)          } } },
+            { for_primary:      { display_method: ->(row) { show_length(row.columns_primary)       } } },
+            { for_foreign:      { display_method: ->(row) { show_length(row.columns_foreign)       } } },
+            { for_timestamp:    { display_method: ->(row) { show_length(row.columns_timestamp)     } } },
+            { for_deleted_at:   { display_method: ->(row) { show_length(row.columns_deleted_at)    } } },
+            { for_virtual:      { display_method: ->(row) { show_length(row.columns_virtual)       } } },
+            { for_data_foreign: { display_method: ->(row) { show_length(row.columns_data_foreign)  } } }
+          ]
+        },
+        erd_files: { skip: true },
+        dictionary: { skip: true }
+      }
+    end
+
+    it do
+      log.structure(load_domain_model.domain, title: 'Models', line_width: 200, graph: graph)
+    end
+  end
+
+  context 'print erd_files' do
+    let(:graph) do
+      {
+        models: { skip: true },
+        erd_files: {
+          title: 'ERD Files',
+          take: :all,
+          columns: [
+            :name,
+            :name_plural,
+            { dsl_file:                 { display_method: ->(row) { File.basename(row.dsl_file) } } },
+            { ruby:                     { display_method: ->(row) { row.source&.ruby ? 'true' : '' } } },
+            { has_public:               { display_method: ->(row) { row.source&.public ? 'true' : '' } } },
+            { has_private:              { display_method: ->(row) { row.source&.private ? 'true' : '' } } },
+            { class_methods:            { display_method: ->(row) { show_length(row.source&.all_methods&.klass) } } },
+            { instance_methods:         { display_method: ->(row) { show_length(row.source&.all_methods&.instance) } } },
+            { instance_public:          { display_method: ->(row) { show_length(row.source&.all_methods&.instance_public) } } },
+            { instance_private:         { display_method: ->(row) { show_length(row.source&.all_methods&.instance_private) } } },
+            { scopes:                   { display_method: ->(row) { show_length(row.dsl&.scopes) } } },
+            { belongs_to:               { display_method: ->(row) { show_length(row.dsl&.belongs_to) } } },
+            { has_one:                  { display_method: ->(row) { show_length(row.dsl&.has_one) } } },
+            { has_many:                 { display_method: ->(row) { show_length(row.dsl&.has_many) } } },
+            { has_and_belongs_to_many:  { display_method: ->(row) { show_length(row.dsl&.has_and_belongs_to_many) } } },
+            { validate_on:              { display_method: ->(row) { show_length(row.dsl&.validate_on) } } },
+            { validates_on:             { display_method: ->(row) { show_length(row.dsl&.validates_on) } } }
+          ]
+        },
+        dictionary: { skip: true }
+      }
+    end
+
+    it do
+      log.structure(load_domain_model.domain, title: 'ERD Files', line_width: 200, graph: graph)
+    end
+  end
+
+  context 'print dictionary' do
+    let(:graph) do
+      {
+        models: { skip: true },
+        dictionary: {
+          title: 'Dictionary',
+          take: :all,
+          columns: [
+            :name,
+            :type,
+            :label,
+            :segment,
+            :model_count,
+            { models: { width: 80, display_method: ->(row) { row.models.reject { |name| name == '__EFMigrationsHistory' || name.start_with?('asp_') }.join(', ') } } },
+            :type_count,
+            { types: { width: 40, display_method: ->(row) { row.types.join(', ') } } }
+          ]
+        },
+        erd_files: { skip: true }
+      }
+    end
+
+    it do
+      log.structure(load_domain_model.domain, title: 'Dictionary', line_width: 200, graph: graph)
+    end
+  end
+end
